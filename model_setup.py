f8e562e51a7474f75496adda95c380119d2cacb9
Alessandro Liuzzi <61493515+alessandroliuzzi@users.noreply.github.com>
2025-12-23 00:07:17 +0100

    Update model_setup.py

--- model_setup.py
+++ model_setup.py
@@ -1,1 +1,12 @@
+from transformers import BertTokenizerFast, BertForSequenceClassification
+
+checkpoint = "bert-base-multilingual-cased"
+
+# Scarica e salva il tokenizer
+tokenizer = BertTokenizerFast.from_pretrained(checkpoint)
+tokenizer.save_pretrained("./bert-cache")
+
+# Scarica e salva il modello
+model = BertForSequenceClassification.from_pretrained(checkpoint, num_labels=2)
+model.save_pretrained("./bert-cache")
 
